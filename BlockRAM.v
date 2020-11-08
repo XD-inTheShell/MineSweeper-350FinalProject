a7d6e8d2ec55c9892dece74b850ad423cdeff8c9
Xiying Deng <xiying.deng@duke.edu>
2020-11-08 15:21:27 -0500

    BlockRAM MOD Done, needs to connect to regfile

--- BlockRAM.v
+++ BlockRAM.v
@@ -6,7 +6,10 @@ module BlockRAM #( parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 4096, 
     input wire [ADDRESS_WIDTH-1:0] addr2,
     input wire [DATA_WIDTH-1:0]    dataIn,
     output wire [DATA_WIDTH-1:0]    dataOut1,
-    output wire [DATA_WIDTH-1:0]    dataOut2); //VGA read
+    output wire [DATA_WIDTH-1:0]    dataOut2,
+    input wire [31:0] checkID,
+    input wire nowCheck
+    ); //VGA read
     
     reg[DATA_WIDTH-1:0] MemoryArray[0:DEPTH-1];
 
@@ -16,35 +19,40 @@ module BlockRAM #( parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 4096, 
         end
     end
     
+    assign isBomb = MemoryArray[checkID]==32'd11;
     always @(negedge clk) begin
         if(wEn) begin
             MemoryArray[addr1] = dataIn;
         end
+        if(nowCheck) begin
+            MemoryArray[checkID] = numArray[checkID];
+        end
+
     end
 
     assign dataOut1 = MemoryArray[addr1];
     assign dataOut2 = MemoryArray[addr2];
 
-    wire [31:0] numArray[4:0][4:0];
+    wire [31:0] numArray[24:0];
 
-    assign numArray[0][0] = (MemoryArray[1]==32'd10) + (MemoryArray[6]==32'd10) + (MemoryArray[5]==32'd10);
-    assign numArray[0][4] = (MemoryArray[3]==32'd10) + (MemoryArray[8]==32'd10) + (MemoryArray[9]==32'd10);
-    assign numArray[4][0] = (MemoryArray[15]==32'd10) + (MemoryArray[16]==32'd10) + (MemoryArray[21]==32'd10);
-    assign numArray[4][4] = (MemoryArray[18]==32'd10) + (MemoryArray[19]==32'd10) + (MemoryArray[23]==32'd10);
+    assign numArray[0] = (MemoryArray[1]==32'd10) + (MemoryArray[6]==32'd10) + (MemoryArray[5]==32'd10);
+    assign numArray[4] = (MemoryArray[3]==32'd10) + (MemoryArray[8]==32'd10) + (MemoryArray[9]==32'd10);
+    assign numArray[20] = (MemoryArray[15]==32'd10) + (MemoryArray[16]==32'd10) + (MemoryArray[21]==32'd10);
+    assign numArray[24] = (MemoryArray[18]==32'd10) + (MemoryArray[19]==32'd10) + (MemoryArray[23]==32'd10);
 
     genvar i;
     generate
         for(i=1; i<4; i=i+1) begin: loopi
-            assign numArray[0][i] = (MemoryArray[i-1]==32'd10) + (MemoryArray[i+1]==32'd10) + (MemoryArray[i+5]==32'd10)+ (MemoryArray[i+4]==32'd10)+ (MemoryArray[i+6]==32'd10);
-            assign numArray[4][i] = (MemoryArray[20+i-1]==32'd10) + (MemoryArray[20+i+1]==32'd10) + (MemoryArray[20+i-5]==32'd10)+ (MemoryArray[20+i-4]==32'd10)+ (MemoryArray[20+i-6]==32'd10);
+            assign numArray[i] = (MemoryArray[i-1]==32'd10) + (MemoryArray[i+1]==32'd10) + (MemoryArray[i+5]==32'd10)+ (MemoryArray[i+4]==32'd10)+ (MemoryArray[i+6]==32'd10);
+            assign numArray[20+i] = (MemoryArray[20+i-1]==32'd10) + (MemoryArray[20+i+1]==32'd10) + (MemoryArray[20+i-5]==32'd10)+ (MemoryArray[20+i-4]==32'd10)+ (MemoryArray[20+i-6]==32'd10);
         end 
     endgenerate
 
     genvar j;
     generate
         for(j=1; j<4; j=j+1) begin: loopj
-            assign numArray[j][0] = (MemoryArray[5*(j-1)]==32'd10) + (MemoryArray[5*(j+1)]==32'd10)+ (MemoryArray[5*(j-1)+1]==32'd10)+ (MemoryArray[5*(j+1)+1]==32'd10) + (MemoryArray[5*j+1]==32'd10);
-            assign numArray[j][4] = (MemoryArray[5*(j-1)+4]==32'd10) + (MemoryArray[5*(j+1)+4]==32'd10)+ (MemoryArray[5*(j-1)+3]==32'd10)+ (MemoryArray[5*(j+1)+3]==32'd10) + (MemoryArray[5*j+3]==32'd10);
+            assign numArray[j*5] = (MemoryArray[5*(j-1)]==32'd10) + (MemoryArray[5*(j+1)]==32'd10)+ (MemoryArray[5*(j-1)+1]==32'd10)+ (MemoryArray[5*(j+1)+1]==32'd10) + (MemoryArray[5*j+1]==32'd10);
+            assign numArray[j*5+4] = (MemoryArray[5*(j-1)+4]==32'd10) + (MemoryArray[5*(j+1)+4]==32'd10)+ (MemoryArray[5*(j-1)+3]==32'd10)+ (MemoryArray[5*(j+1)+3]==32'd10) + (MemoryArray[5*j+3]==32'd10);
         end 
     endgenerate
 
@@ -52,7 +60,7 @@ module BlockRAM #( parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 4096, 
     generate
         for(m=1; m<4; m=m+1) begin: loopm
             for(n=1; n<4; n=n+1) begin: loopn
-            assign numArray[m][n] = (MemoryArray[5*(m-1)+n]==32'd10) + (MemoryArray[5*(m+1)+n]==32'd10)+ (MemoryArray[5*(m-1)+n+1]==32'd10)+ (MemoryArray[5*(m+1)+n+1]==32'd10) + (MemoryArray[5*m+n+1]==32'd10)+ (MemoryArray[5*(m-1)+n-1]==32'd10)+ (MemoryArray[5*(m+1)+n-1]==32'd10) + (MemoryArray[5*m+n-1]==32'd10);
+            assign numArray[m*5+n] = (MemoryArray[5*(m-1)+n]==32'd10) + (MemoryArray[5*(m+1)+n]==32'd10)+ (MemoryArray[5*(m-1)+n+1]==32'd10)+ (MemoryArray[5*(m+1)+n+1]==32'd10) + (MemoryArray[5*m+n+1]==32'd10)+ (MemoryArray[5*(m-1)+n-1]==32'd10)+ (MemoryArray[5*(m+1)+n-1]==32'd10) + (MemoryArray[5*m+n-1]==32'd10);
             end
         end 
     endgenerate
--- BlockRAM_tb.v
+++ BlockRAM_tb.v
@@ -3,15 +3,27 @@ module BlockRAM_tb();
     reg                     wEn;
     reg [11:0] addr1;
     reg [11:0] addr2;
-    reg [31:0]    dataIn;
+    reg [31:0]    dataIn, checkID;
+    reg nowCheck;
 
-    BlockRAM testRAM(.clk(clk), .wEn(wEn), .addr1(addr1), .addr2(addr2), .dataIn(dataIn));
+    BlockRAM testRAM(.clk(clk), .wEn(wEn), .addr1(addr1), .addr2(addr2), .dataIn(dataIn), .checkID(checkID), .nowCheck(nowCheck));
     initial begin
         clk = 0;
         wEn = 0;
         addr1 = 12'b0;
         addr2 = 12'b0;
         dataIn = 31'b0;
+        nowCheck = 0;
+        checkID = 32'd5;
+
+        #20
+        nowCheck = 1;
+        #30
+        nowCheck = 0;
+        #40
+        checkID = 32'd16;
+        #60
+        nowCheck = 1;
 
         //Set a time delay, in nanoseconds
         #8000;
@@ -38,31 +50,57 @@ module BlockRAM_tb();
         //         $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[i][j]);
         //     end
         // end
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[0][0]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[0][1]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[0][2]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[0][3]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[0][4]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[1][0]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[1][1]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[1][2]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[1][3]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[1][4]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[2][0]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[2][1]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[2][2]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[2][3]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[2][4]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[3][0]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[3][1]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[3][2]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[3][3]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[3][4]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[4][0]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[4][1]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[4][2]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[4][3]);
-        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[4][4]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[0]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[1]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[2]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[3]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[4]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[5]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[6]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[7]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[8]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[9]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[10]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[11]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[12]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[13]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[14]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[15]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[16]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[17]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[18]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[19]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[20]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[21]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[22]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[23]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.MemoryArray[24]);
+
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[0]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[1]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[2]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[3]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[4]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[5]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[6]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[7]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[8]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[9]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[10]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[11]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[12]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[13]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[14]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[15]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[16]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[17]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[18]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[19]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[20]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[21]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[22]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[23]);
+        $dumpvars(0, BlockRAM_tb.testRAM.BlockRAM.numArray[24]);
         $dumpvars(0, BlockRAM_tb);
     end
 endmodule
